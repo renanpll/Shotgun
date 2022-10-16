f0d582cc8bad896c9e76edf6abe13caef780bdc1
renanpll <renanpll@gmail.com>
2022-10-16 11:51:40 -0400

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Shotgun"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Shotgun"
+	location "Shotgun"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SG_PLATFORM_WINDOWS",
+			"SG_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputDir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "SG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SG_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SG_Dist"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Shotgun/vendor/spdlog/include",
+		"Shotgun/src"
+	}
+
+	links
+	{
+		"Shotgun"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SG_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SG_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SG_Dist"
+		optimize "On"
